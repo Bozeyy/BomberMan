a5bf31518b2f6d09faca3e1d59b6261894033868
Bozeyy <spereaux2@gmail.com>
2022-10-11 13:25:14 +0200

    ajout du logo dans la racine

--- js/main.js
+++ js/main.js
@@ -44,10 +44,6 @@ imgBomb.src = "img/PinkBomb.png";
 
 
 
-
-
-
-
 // faire un tableau a deux dimensions
 var tab = new Array(11);
 for (var i = 0; i < tab.length; i++) {
@@ -513,4 +509,7 @@ function explosionCase(x, y) {
         console.log("dehors");
     }
 }
+
+
+
 
